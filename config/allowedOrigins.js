b6ecefdf8455b918c63ec08a9e468b5ed782e234
Nikolas Furuta <nikolasfuruta@gmail.com>
2023-02-14 20:56:37 -0300

    fix cors 6

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3500',
   'http://127.0.0.1:5500',
-  'chic-panda-e6c535'
+  'https://nikolasfuruta.github.io/frontend_login_project/'
 ];
 
 module.exports = allowedOrigins;
