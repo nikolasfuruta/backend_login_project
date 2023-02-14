3cdc97e4293a4a58bac7d132c8ac8b72ea78f5b2
Nikolas Furuta <nikolasfuruta@gmail.com>
2023-02-14 20:11:18 -0300

    fix cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3500',
   'http://127.0.0.1:5500',
+  'https://chic-panda-e6c535.netlify.app'
 ];
 
 module.exports = allowedOrigins;
