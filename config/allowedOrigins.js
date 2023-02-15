52ced65245a76f9be6bfc11a503cb30fd0e671b8
Nikolas Furuta <nikolasfuruta@gmail.com>
2023-02-15 16:58:59 -0300

    add github page into white list

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3500',
   'http://127.0.0.1:5500',
-  'https://nikolasfuruta.github.io/frontend_login_project/'
+  'https://nikolasfuruta.github.io'
 ];
 
 module.exports = allowedOrigins;
